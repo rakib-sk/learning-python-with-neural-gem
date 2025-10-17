a9bc941606c7a40f0f95ec7f8b43e8f0ff20a895
Rakib Hoseen <rh01828315879@gmail.com>
2025-10-17 11:45:07 +0600

    Home work 1

---
+++ Home work/qustion1.py
@@ -0,0 +1,15 @@
+'''Write a Python script that asks the user for three integer inputs: a, b, and c. Calculate the
+result of (a ** b). Then, find the floor division of this result by c. Finally, find the modulus of the
+floor division result with the number a. Print the final answer.'''
+#
+# 
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+#power calulate a and b
+pow = a ** b
+#floor division pow and c
+flr_div = pow // c
+#Modulus division result with a
+modulus = flr_div%a
+print("Result is: ",modulus)
